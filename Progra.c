e46fd2b77751bdf7fdd5a3e1d777532ee1d8cec1
Alejandro Garita <luisgc2009@gmail.com>
2018-04-08 21:05:43 -0600

    Pruebas iniciales de dibujo
    Está pegado con cinta, no es lo ideal, pero es un comienzo.

---
+++ Progra.c
@@ -0,0 +1,106 @@
+#include <math.h>
+#include <stdio.h>
+#include <gtk/gtk.h>
+
+#define WIDTH   640
+#define HEIGHT  480
+
+#define ZOOM_X  100.0
+#define ZOOM_Y  100.0
+
+
+/// VARIABLES GLOBALES
+GtkWidget   *g_winPrincipal;
+GtkWidget 	*g_areaPintado;
+cairo_t 		*cr;
+cairo_surface_t *surface;
+
+///Botón Generar
+gfloat f (gfloat x)
+{
+    return 0.03 * pow (x, 3);
+}
+
+void on_draw (GtkWidget *widget, cairo_t *cr, gpointer user_data)
+{
+    GdkRectangle da;            /* GtkDrawingArea size */
+    gdouble i;
+    gdouble dx = 1.0; 
+    gdouble dy = 1.0; 			/* Pixels between each point */
+    gdouble clip_x1 = 0.0, clip_x2 = 0.0; 
+    gdouble clip_y1 = 0.0, clip_y2 = 0.0;
+    GdkWindow *window = gtk_widget_get_window(widget);
+
+    /* Determine GtkDrawingArea dimensions */
+    gdk_window_get_geometry (window, &da.x, &da.y, &da.width, &da.height);
+
+    /* Draw on a black background */
+    cairo_set_source_rgb (cr, 1.0, 1.0, 1.0);
+    cairo_paint (cr);
+
+    /* Change the transformation matrix */
+    cairo_translate (cr, da.width / 2, da.height / 2);
+    cairo_scale (cr, ZOOM_X, -ZOOM_Y);
+
+    /* Determine the data points to calculate (ie. those in the clipping zone */
+    cairo_device_to_user_distance (cr, &dx, &dy);
+    cairo_clip_extents (cr, &clip_x1, &clip_y1, &clip_x2, &clip_y2);
+    cairo_set_line_width (cr, dx);
+
+    /* Draws x and y axis */
+    cairo_set_source_rgb (cr, 0.0, 1.0, 0.0);
+    cairo_move_to (cr, clip_x1, 0.0);
+    cairo_line_to (cr, clip_x2, 0.0);
+    cairo_move_to (cr, 0.0, clip_y1);
+    cairo_line_to (cr, 0.0, clip_y2);
+    cairo_stroke (cr);
+
+    // Link each data point
+    for (i = clip_x1; i < clip_x2; i += dx)
+        cairo_line_to (cr, i, f (i));
+
+    // Draw the curve
+    cairo_set_source_rgba (cr, 1, 0.2, 0.2, 0.6);
+    cairo_stroke (cr);
+    gtk_window_resize(GTK_WINDOW(g_winPrincipal), WIDTH, HEIGHT);
+}
+
+void on_btnGenerar_clicked(){
+	gtk_widget_show(g_areaPintado);
+}
+
+///Botón Leer
+
+///Botón Salir
+void on_winPrincipal_destroy () {
+	gtk_main_quit();
+}
+
+
+///INTERFAZ
+void crearInterfaz(){
+	GtkBuilder  *builder;
+	builder = gtk_builder_new();
+	gtk_builder_add_from_file (builder, "Interfaz.glade", NULL);
+	
+	g_winPrincipal 	= GTK_WIDGET(gtk_builder_get_object(builder, "winPrincipal"));
+	g_areaPintado 	= GTK_WIDGET(gtk_builder_get_object(builder, "areaPintado"));
+	
+	g_signal_connect (g_areaPintado, "draw", G_CALLBACK (on_draw), NULL);
+	
+	gtk_builder_connect_signals(builder, NULL);
+	g_object_unref(builder);
+    gtk_widget_show_all(g_winPrincipal);
+    gtk_widget_hide(g_areaPintado);             
+    gtk_main();
+}
+
+///MAIN
+int main(int argc, char **argv)
+{
+	gtk_init(&argc, &argv);
+	crearInterfaz();
+	
+	return 0;
+}
+
