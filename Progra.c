071af5f639f731e08dc5615c214e62f82256f792
Alejandro Garita <luisgc2009@gmail.com>
2018-04-17 15:21:02 -0600

    Matriz vacia
    Ya se puede hacer matrices de n elementos (solo vacias).

--- Progra.c
+++ Progra.c
@@ -15,16 +15,17 @@ GtkWidget 	*g_areaPintado;
 cairo_t 		*cr;
 cairo_surface_t *surface;
 
-///Botón Generar
-gfloat f (gfloat x)
-{
-    return 0.03 * pow (x, 3);
+void piso(){
+	
+}
+
+void pared(){
+	
 }
 
 void on_draw (GtkWidget *widget, cairo_t *cr, gpointer user_data)
 {
     GdkRectangle da;            /* GtkDrawingArea size */
-    gdouble i;
     gdouble dx = 1.0; 
     gdouble dy = 1.0; 			/* Pixels between each point */
     gdouble clip_x1 = 0.0, clip_x2 = 0.0; 
@@ -45,28 +46,51 @@ void on_draw (GtkWidget *widget, cairo_t *cr, gpointer user_data)
     /* Determine the data points to calculate (ie. those in the clipping zone */
     cairo_device_to_user_distance (cr, &dx, &dy);
     cairo_clip_extents (cr, &clip_x1, &clip_y1, &clip_x2, &clip_y2);
-    cairo_set_line_width (cr, dx);
-
+    
+    
+	
+	
     /* Draws x and y axis */
-    cairo_set_source_rgb (cr, 0.0, 1.0, 0.0);
-    cairo_move_to (cr, clip_x1, 0.0);
-    cairo_line_to (cr, clip_x2, 0.0);
-    cairo_move_to (cr, 0.0, clip_y1);
-    cairo_line_to (cr, 0.0, clip_y2);
-    cairo_stroke (cr);
+    /*cairo_set_line_width (cr, 0.01);
+    cairo_set_source_rgb (cr, 0, 0, 0);
+    cairo_rectangle (cr, clip_x1, clip_y1, 0.3, 0.3);			//Esquina inferior izquierda
+    cairo_set_source_rgb (cr, 0.2, 0.2, 0.2);
+    cairo_rectangle (cr, clip_x2-0.3, clip_y1, 0.3, 0.3);		//Esquina inferior derecha
+    cairo_set_source_rgb (cr, 0.2, 0.4, 0.4);
+    cairo_rectangle (cr, clip_x1, clip_y2-0.3, 0.3, 0.3); 		//Esquina superior izquierda
+    cairo_set_source_rgb (cr, 0.2, 0.6, 0.6);
+    cairo_rectangle (cr, clip_x2-0.3, clip_y2-0.3, 0.3, 0.3);	//Esquina superior derecha 
+	cairo_stroke (cr);*/
+	cairo_set_line_width (cr, 0.01);
+    cairo_set_source_rgb (cr, 0.0, 0.0, 0.0);
+    int c = 1;
+    int n = 100; 						
+    double w = clip_x2*2/(n);
+    double t = clip_y2*2/(n)+ 0.0001;
+    double p = clip_x2;
+    double q = clip_y2;
+    printf("\nCon n = %d, hay un w = %f y un t = %f\n", n, w, t);
+    printf("Clips x: %f, %f\n", clip_x1, clip_x2);
+    printf("Clips y: %f, %f\n\n", clip_y1, clip_y2);
+    for(double x = clip_x1; x <= p || c == 40; x += w){
+		for(double y = clip_y1; y <= q || c == 40; y += t){
+			printf("Posicion %d: %f = %f, %f = %f\n", c, x, p, y, q);
+			cairo_rectangle (cr, x, y, w, t);
+			c++;
+		}
+	}
+	printf("Salio");
+	cairo_stroke (cr);
+    
+    //cairo_move_to (cr, 0.0, clip_y1);
+    //cairo_line_to (cr, 0.0, clip_y2);
+    
 
-    // Link each data point
-    for (i = clip_x1; i < clip_x2; i += dx)
-        cairo_line_to (cr, i, f (i));
-
-    // Draw the curve
-    cairo_set_source_rgba (cr, 1, 0.2, 0.2, 0.6);
-    cairo_stroke (cr);
     gtk_window_resize(GTK_WINDOW(g_winPrincipal), WIDTH, HEIGHT);
 }
 
 void on_btnGenerar_clicked(){
-	gtk_widget_show(g_areaPintado);
+	
 }
 
 ///Botón Leer
@@ -76,6 +100,11 @@ void on_winPrincipal_destroy () {
 	gtk_main_quit();
 }
 
+/// Zoom in
+
+
+
+/// Zoom out
 
 ///INTERFAZ
 void crearInterfaz(){
@@ -87,11 +116,12 @@ void crearInterfaz(){
 	g_areaPintado 	= GTK_WIDGET(gtk_builder_get_object(builder, "areaPintado"));
 	
 	g_signal_connect (g_areaPintado, "draw", G_CALLBACK (on_draw), NULL);
+	//gtk_widget_show(g_areaPintado);
 	
 	gtk_builder_connect_signals(builder, NULL);
 	g_object_unref(builder);
     gtk_widget_show_all(g_winPrincipal);
-    gtk_widget_hide(g_areaPintado);             
+    //gtk_widget_hide(g_areaPintado);             
     gtk_main();
 }
 
