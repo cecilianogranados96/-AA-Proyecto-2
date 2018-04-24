da1096c824250efce5a5e5bfd71f8cf296afe49b
Alejandro Garita <luisgc2009@gmail.com>
2018-04-23 18:44:52 -0600

    Generar actualizado
    Ya se pueden generar cuantos laberintos se quieran

--- Progra.c
+++ Progra.c
@@ -17,84 +17,127 @@ int 		g_width;
 int			g_height;
 int         c = 0; //TAMAÑO PARA ARCHIVO DE GUARDAR Y LEER
 
-int **laberinto,**spanningTree,*frontierR,*frontierC,n=-1,counter=-1;
+int **laberinto, **arbolExpansion;
+int *filaFrontera, *columnaFrontera;
+int n=-1, contador=-1;
 
-void addAdjacent(int rowIndex,int columnIndex){
-	if(laberinto[rowIndex][columnIndex]==0){
+///---------------------------------------------------------------------
+/// Botón Generar
+void agregarAdyacente(int filaIndice,int columnaIndice){
+	if(!laberinto[filaIndice][columnaIndice]){
 		n++;
-		laberinto[rowIndex][columnIndex]=-1;
-		frontierR[n]=rowIndex;
-		frontierC[n]=columnIndex;
+		laberinto[filaIndice][columnaIndice] =- 1;
+		filaFrontera[n] = filaIndice;
+		columnaFrontera[n] = columnaIndice;
 	}
 }
 
-void markAdjacent(int rowIndex,int columnIndex){
-	if(rowIndex!=0){
-		addAdjacent(rowIndex-1,columnIndex);
-	}
-	if(rowIndex!=g_width-1){
-		addAdjacent(rowIndex+1,columnIndex);	
-	}
-	if(columnIndex!=0){
-		addAdjacent(rowIndex,columnIndex-1);
-	}
-	if(columnIndex!=g_height-1){
-		addAdjacent(rowIndex,columnIndex+1);
+void marcarAdyacente(int filaIndice,int columnaIndice){
+	if(filaIndice)
+		agregarAdyacente(filaIndice-1, columnaIndice);
+	if(filaIndice != g_width-1)
+		agregarAdyacente(filaIndice+1, columnaIndice);	
+	if(columnaIndice)
+		agregarAdyacente(filaIndice, columnaIndice-1);
+	if(columnaIndice != g_height-1)
+		agregarAdyacente(filaIndice, columnaIndice+1);
+}
+
+void esAdyacente(int filaIndice,int columnaIndice,int filaAdyacente[],int columnaAdyacente[]){
+	if((laberinto[filaIndice][columnaIndice] == 1)||
+	   (laberinto[filaIndice][columnaIndice] ==16)){
+		contador++;
+		filaAdyacente[contador] = filaIndice;
+		columnaAdyacente[contador] = columnaIndice;
 	}
 }
 
-void isAdjacent(int indexR,int indexC,int adjacentR[],int adjacentC[]){
-	if((laberinto[indexR][indexC]==1)||(laberinto[indexR][indexC]==16)){
-		counter++;
-		adjacentR[counter]=indexR;
-		adjacentC[counter]=indexC;
+void asignarConexion(){
+	int k,adyacenteAConectar,posFronteraF,posFronteraC;
+	int filaAdyacente[4],columnaAdyacente[4];
+	
+	if(!n) k = 0;
+	else k = rand()%n;
+		
+	posFronteraF = filaFrontera[k];
+	posFronteraC = columnaFrontera[k];
+	contador = -1;
+	
+	if(posFronteraF)
+		esAdyacente(posFronteraF-1, posFronteraC, filaAdyacente, columnaAdyacente);
+	if(posFronteraF != g_width-1)
+		esAdyacente(posFronteraF+1, columnaFrontera[k], filaAdyacente, columnaAdyacente);
+	if(posFronteraC)
+		esAdyacente(posFronteraF, columnaFrontera[k]-1, filaAdyacente, columnaAdyacente);
+	if(posFronteraC != g_height-1)
+		esAdyacente(posFronteraF, columnaFrontera[k]+1, filaAdyacente, columnaAdyacente);
+	if(!contador) adyacenteAConectar = 0;
+	
+	else adyacenteAConectar=rand()%contador;
+	
+	if(filaAdyacente[adyacenteAConectar] == posFronteraF-1){
+		arbolExpansion[filaAdyacente[adyacenteAConectar]][columnaAdyacente[adyacenteAConectar]] += 2;
+		arbolExpansion[posFronteraF][posFronteraC] += 8;
 	}
-}
-
-void asignConection(){
-	int k,adjacentR[4],adjacentC[4],adjacentToConnect,frontierCellR,frontierCellC;
-	if(n==0){
-		k=0;
-	}else{
-		k=rand()%n;
+	else if(filaAdyacente[adyacenteAConectar] == posFronteraF+1){
+		arbolExpansion[filaAdyacente[adyacenteAConectar]][columnaAdyacente[adyacenteAConectar]] += 8;
+		arbolExpansion[posFronteraF][posFronteraC] += 2;
 	}
-	frontierCellR=frontierR[k],frontierCellC=frontierC[k];
-	counter=-1;
-	if(frontierCellR!=0){
-		isAdjacent(frontierCellR-1,frontierCellC,adjacentR,adjacentC);
+	else if(columnaAdyacente[adyacenteAConectar] == posFronteraC+1){
+		arbolExpansion[filaAdyacente[adyacenteAConectar]][columnaAdyacente[adyacenteAConectar]] += 4;
+		arbolExpansion[posFronteraF][posFronteraC] += 1;
+	} 
+	else{
+		arbolExpansion[filaAdyacente[adyacenteAConectar]][columnaAdyacente[adyacenteAConectar]] += 1;
+		arbolExpansion[posFronteraF][posFronteraC] += 4;
 	}
-	if(frontierCellR!=g_width-1){
-		isAdjacent(frontierCellR+1,frontierC[k],adjacentR,adjacentC);	
-	}
-	if(frontierCellC!=0){
-		isAdjacent(frontierCellR,frontierC[k]-1,adjacentR,adjacentC);
-	}
-	if(frontierCellC!=g_height-1){
-		isAdjacent(frontierCellR,frontierC[k]+1,adjacentR,adjacentC);
-	}
-	if(counter==0){
-		adjacentToConnect=0;
-	}else{
-		adjacentToConnect=rand()%counter;
-	}
-	if(adjacentR[adjacentToConnect]==frontierCellR-1){
-		spanningTree[adjacentR[adjacentToConnect]][adjacentC[adjacentToConnect]]+=2;
-		spanningTree[frontierCellR][frontierCellC]+=8;
-	}else if(adjacentR[adjacentToConnect]==frontierCellR+1){
-		spanningTree[adjacentR[adjacentToConnect]][adjacentC[adjacentToConnect]]+=8;
-		spanningTree[frontierCellR][frontierCellC]+=2;
-	}else if(adjacentC[adjacentToConnect]==frontierCellC+1){
-		spanningTree[adjacentR[adjacentToConnect]][adjacentC[adjacentToConnect]]+=4;
-		spanningTree[frontierCellR][frontierCellC]+=1;
-	} else{
-		spanningTree[adjacentR[adjacentToConnect]][adjacentC[adjacentToConnect]]+=1;
-		spanningTree[frontierCellR][frontierCellC]+=4;
-	}
-	laberinto[frontierCellR][frontierCellC]=1;
-	markAdjacent(frontierCellR,frontierCellC);
-	frontierR[k]=frontierR[n];
-	frontierC[k]=frontierC[n];
+	
+	laberinto[posFronteraF][posFronteraC] = 1;
+	marcarAdyacente(posFronteraF,posFronteraC);
+	filaFrontera[k] = filaFrontera[n];
+	columnaFrontera[k] = columnaFrontera[n];
 	n--;
+}
+
+void generarLaberinto(){
+	int i,j,randomR,randomC;
+    
+    columnaFrontera	= (int *)malloc(g_width*g_height*sizeof(int));
+	filaFrontera	= (int *)malloc(g_width*g_height*sizeof(int));
+	arbolExpansion	= (int **)malloc(g_width*sizeof(int *));
+	laberinto	 	= (int **)malloc(g_width*sizeof(int *));
+	
+	for (i = 0; i < g_width; i++){
+		laberinto[i]	= (int *)malloc(g_height*sizeof(int));
+		arbolExpansion[i]	= (int *)malloc(g_height*sizeof(int));
+	}
+  	
+  	for (i = 0; i < g_width; i++){
+      	for (j = 0; j < g_height; j++){
+			laberinto[i][j]	= 0;
+			arbolExpansion[i][j] = 0;
+		}
+	}
+	
+	randomR	= rand()%g_width;
+	randomC	= rand()%g_height;
+	laberinto[randomR][randomC] = 16;
+	
+	marcarAdyacente(randomR,randomC);
+	
+	asignarConexion();
+	
+	while(n != -1)
+		asignarConexion();
+
+
+	//ARBOL DE EXPANCION
+	for (i = 0;i < g_width;i++){
+ 		for (j = 0;j < g_height;j++)
+			printf("%d\t",arbolExpansion[i][j]);
+		printf("\n");
+    }
+
 }
 
 void on_draw (GtkWidget *widget, cairo_t *cr, gpointer user_data){
@@ -105,115 +148,47 @@ void on_draw (GtkWidget *widget, cairo_t *cr, gpointer user_data){
 	float x_scaling, y_scaling;
 	float tex_width, tex_height;
 	int filas = 0, columnas = 0;
+	
 	window = gtk_widget_get_window(widget);
 	piso = cairo_image_surface_create_from_png ("data/piso.png");
-	tex_width 	= cairo_image_surface_get_width (piso);					//Las dos son del mismo tamaño así que ambas imagenes sirven
+	tex_width 	= cairo_image_surface_get_width (piso);
 	tex_height 	= cairo_image_surface_get_height (piso);
 	gdk_window_get_geometry (window, &da.x, &da.y, &da.width, &da.height);	// Determinamos el tamaño de la drawing area y la ponemos en el 
 	cairo_translate (cr, da.width / 2, da.height / 2);						// centro del widget.
-	p = (float)-(16*g_width);		//Inicio de x
-	q = (float)-(16*g_height);		//Inicio de y
+	
+	q = (float)16*g_width;		//Inicio de x
+	p = (float)16*g_height;		//Inicio de y
 	w = (float)da.width/g_width;	//Ancho de cada cuadrito
 	t = (float)da.height/g_height;	//Largo de cada cuadrito
-	x_scaling = w / tex_width;
-	y_scaling = t / tex_height;
+	x_scaling = (w / tex_width);
+	y_scaling = (t / tex_height);
 	cairo_scale (cr, x_scaling, y_scaling);
-	cairo_set_line_width(cr, 1); 
-    
-    int i,j,randomR,randomC;
-    
-	frontierR=(int *)malloc(g_width*g_height*sizeof(int));
-	frontierC=(int *)malloc(g_width*g_height*sizeof(int));
-	spanningTree=(int **)malloc(g_width*sizeof(int *)); // OJO CON ESTO AL LEER
-	laberinto=(int **)malloc(g_width*sizeof(int *));
-	for (i = 0; i < g_width; i++){
-		laberinto[i]=(int *)malloc(g_height*sizeof(int));
-		spanningTree[i]=(int *)malloc(g_height*sizeof(int));
-	}
-  	for (i = 0; i < g_width; i++){
-      		for (j = 0; j < g_height; j++){
-			laberinto[i][j]=0;
-			spanningTree[i][j]=0;
-		}
-	}
-	randomR=rand()%g_width;
-	randomC=rand()%g_height;
-	laberinto[randomR][randomC]=16;
-	markAdjacent(randomR,randomC);
-	asignConection();
-	while(n!=-1){
-		asignConection();
-	}
-
-    
-    /*
-	//ARBOL DE EXPACION
-	for (i = 0;i<g_width;i++){
- 		for (j = 0;j<g_height;j++){
-			printf("%d\t",spanningTree[i][j]);
-		}
-		printf("\n");
-    }
-    */
+	cairo_set_line_width(cr, 0); 
     
     
-    char buf[1024];
-	for(double x = p; x < -p; x += 32){						//Las imagenes son de 32x32
-		for(double y = q; y < -q; y += 32){		
-            //printf("C:%d F:%d ",columnas,filas);
+    char buf[2048];
     
-                    
-                    
-            if ((columnas == 0 && filas == 0)) { //SACA ESQUINA SUPERIOR
+	for(double x = -p; x < p; x += 32){						//Las imagenes son de 32x32
+		for(double y = -q; y < q; y += 32){
+			//printf("R: %d y C: %d\n",filas, columnas);
+			if ((columnas == 0 && filas == 0)) 				//SACA ESQUINA SUPERIOR
                 sprintf(buf, "data/%d.png", 7);
-            }else{
-                if (((columnas == (g_width-1)) &&  (filas == (g_height-1)))){ //SACA ESQUINA INFERIOR
-                    sprintf(buf, "data/%d.png", 13);
-                }else{
-                    sprintf(buf, "data/%d.png", spanningTree[columnas][filas]); //EL RESTO ESCRIBE LO QUE TINENE EL ARBOL DE EXPANCION
-                }
-            }
-            
+            else if(columnas == (g_width-1) && filas == (g_height-1)) //SACA ESQUINA INFERIOR
+                sprintf(buf, "data/%d.png", 13);
+            else
+                sprintf(buf, "data/%d.png", arbolExpansion[filas][columnas]); //EL RESTO ESCRIBE LO QUE TINENE EL ARBOL DE EXPANSION
+            printf("\nfilas: %d columnas: %d\n", filas, columnas);
             pared = cairo_image_surface_create_from_png(buf);
+            printf("x: %f y: %f\t", x, y);
             cairo_set_source_surface(cr, pared, x, y);
             cairo_paint(cr);
-            columnas++;
+            filas++;
 		}
-        //printf("\n");
-		filas++;
-		columnas = 0;
+		printf("\n");
+		filas = 0;
+		columnas++;
 	}
     
-}
-///---------------------------------------------------------------------
-void leer (char nombre[1024]) {
-	int i = 0, j = 0;
-    char linea[1024];
-    FILE *archivo = fopen(nombre, "r");
-    
-    while(fgets(linea, 1024, archivo)) {
-        char * pch = strtok (linea," ,");
-        while (pch != NULL){
-            spanningTree[i][j] =  atoi(pch);
-            pch = strtok (NULL, " ,"); 
-            j++;
-        } i++; j = 0;
-    }
-    fclose(archivo);
-    
-    //AQUI DEBERIA LLAMAR A ON DRAW    
-}
-void guardar (char nombre[1024]) {
-	FILE* fichero;
-	fichero = fopen(nombre, "w+");
-    for(int i = 0; i < g_width; i++) {
-        for(int j = 0; j < g_height; j++) {
-            fprintf(fichero, "%d,", spanningTree[i][j]);       
-        }
-        fprintf(fichero, "\n");
-	}
-	fclose(fichero);
-	printf("\nProceso completado");
 }
 
 void on_btnGenerar_clicked(){
@@ -223,12 +198,13 @@ void on_btnGenerar_clicked(){
 										  GTK_DIALOG_MODAL|GTK_DIALOG_DESTROY_WITH_PARENT, 
 										  "_Aceptar" , GTK_RESPONSE_ACCEPT, 
 										  "_Cancelar", GTK_RESPONSE_REJECT, NULL);
-	
 	caja = GTK_WIDGET(gtk_dialog_get_content_area(GTK_DIALOG(dialog)));
-	lblFil = gtk_label_new("Filas: ");
-	spinFil = gtk_spin_button_new_with_range (4	, 2048, 1); //OJO QUITAR EL 4 PARA PRODUCCION
-	lblCol = gtk_label_new("Columnas: ");
-	spinCol = gtk_spin_button_new_with_range (4, 2048, 1); //OJO QUITAR EL 4 PARA PRODUCCION
+	
+	lblFil = gtk_label_new("Columnas: ");
+	lblCol = gtk_label_new("Filas: ");
+	spinFil = gtk_spin_button_new_with_range (1	, 2048, 1);
+	spinCol = gtk_spin_button_new_with_range (1, 2048, 1);
+	
 	gtk_container_add(GTK_CONTAINER(caja), lblFil);
 	gtk_container_add(GTK_CONTAINER(caja), spinFil);
 	gtk_container_add(GTK_CONTAINER(caja), lblCol);
@@ -237,16 +213,40 @@ void on_btnGenerar_clicked(){
 	
 	int respuesta = gtk_dialog_run (GTK_DIALOG (dialog));
 	if (respuesta == GTK_RESPONSE_ACCEPT) {
-		g_height = gtk_spin_button_get_value (GTK_SPIN_BUTTON(spinFil));
-		g_width = gtk_spin_button_get_value (GTK_SPIN_BUTTON(spinCol));
-		g_signal_connect (g_areaPintado, "draw", G_CALLBACK (on_draw), NULL);
 		gtk_widget_set_sensitive (btnResolver, TRUE);
 		gtk_widget_set_sensitive (btnGrabar, TRUE);
+		g_height = gtk_spin_button_get_value (GTK_SPIN_BUTTON(spinFil));
+		g_width  = gtk_spin_button_get_value (GTK_SPIN_BUTTON(spinCol));
+		
+		g_signal_connect(g_areaPintado, "draw", G_CALLBACK (on_draw), NULL);
+		//
+		generarLaberinto();
+		//
 	}
-	
+
 	gtk_widget_destroy (dialog);
 }
 
+
+///---------------------------------------------------------------------
+/// Botón leer
+void leer (char nombre[1024]) {
+	int i = 0, j = 0;
+    char linea[1024];
+    FILE *archivo = fopen(nombre, "r");
+    
+    while(fgets(linea, 1024, archivo)) {
+        char * pch = strtok (linea," ,");
+        while (pch != NULL){
+            arbolExpansion[i][j] =  atoi(pch);
+            pch = strtok (NULL, " ,"); 
+            j++;
+        } i++; j = 0;
+    }
+    fclose(archivo);
+    
+    //AQUI DEBERIA LLAMAR A ON DRAW    
+}
 
 void on_btnLeer_clicked(){
 	GtkWidget *dialog = gtk_file_chooser_dialog_new("Leer Archivo", GTK_WINDOW(g_winPrincipal), 
@@ -260,6 +260,22 @@ void on_btnLeer_clicked(){
 		leer(filename);
 	}
 	gtk_widget_destroy (dialog);
+}
+
+
+///---------------------------------------------------------------------
+/// Botón Guardar
+void guardar (char nombre[1024]) {
+	FILE* fichero;
+	fichero = fopen(nombre, "w+");
+    for(int i = 0; i < g_width; i++) {
+        for(int j = 0; j < g_height; j++) {
+            fprintf(fichero, "%d,", arbolExpansion[i][j]);       
+        }
+        fprintf(fichero, "\n");
+	}
+	fclose(fichero);
+	printf("\nProceso completado");
 }
 
 void on_btnGrabar_clicked(){
@@ -277,6 +293,9 @@ void on_btnGrabar_clicked(){
 	gtk_widget_destroy (dialog);
 }
 
+
+///---------------------------------------------------------------------
+/// Botón Resolver
 void on_btnResolver_clicked(){
 	GtkWidget *dialog = gtk_message_dialog_new (GTK_WINDOW(g_winPrincipal), 
 												GTK_DIALOG_DESTROY_WITH_PARENT, 
@@ -286,6 +305,9 @@ void on_btnResolver_clicked(){
 	gtk_widget_destroy (dialog);
 }
 
+
+///---------------------------------------------------------------------
+/// Botón Salir
 void on_winPrincipal_destroy (){
 	gtk_main_quit();
 }
@@ -309,6 +331,8 @@ void on_btnZoomOut_clicked(){
 }
 
 
+///---------------------------------------------------------------------
+/// Interfaz
 void crearInterfaz(){
 	GtkBuilder  *builder;
 	builder = gtk_builder_new();
